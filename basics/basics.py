c6cc7d326f3453597d8d5c3a9a5909c4c2a6ba44
Dave Spina <dspina79@gmail.com>
2022-04-03 13:30:04 -0400

    Refactored print output into methods

--- basics/basics.py
+++ basics/basics.py
@@ -1,25 +1,26 @@
 import numpy as np
 
+def print_array_details(y):
+    print("The sum of the array is ", np.sum(y))
+    print("The mean of the array is ", np.mean(y))
+    print("The median of the array is ", np.median(y))
+    print("The average of the array is ", np.average(y))
+    print("The standard deviation of the array is ", np.std(y))
+    print("The variance of the array is ", np.var(y))
+    print("Remember, the standard deviation is the square root of the variance.")
+    print("The min of the array is ", np.min(y))
+    print("The max of the array is ", np.max(y))
+    print("The index of the min of the array is ", np.argmin(y))
+    print("The index of the min of the array is ", np.argmax(y))
+
+
+
 z = np.zeros(4)
 print(z)
 
 np.random.seed(0)
-y = np.random.randint(10, size=14)
+y = np.random.randint(10, size=25)
 print(y)
 
-avg_y = np.average(y)
-med_y = np.median(y)
-print(avg_y)
-print(med_y)
+print_array_details(y)
 
-# more mathematical functions
-print("The sum of the array is ", np.sum(y))
-print("The mean of the array is ", np.mean(y))
-print("The average of the array is ", np.average(y))
-print("The standard deviation of the array is ", np.std(y))
-print("The variance of the array is ", np.var(y))
-print("Remember, the standard deviation is the square root of the variance.")
-print("The min of the array is ", np.min(y))
-print("The max of the array is ", np.max(y))
-print("The index of the min of the array is ", np.argmin(y))
-print("The index of the min of the array is ", np.argmax(y))
